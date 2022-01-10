6b2a93c9f53e8f29add017a00b4c4ebb7b5d8716
zabiulla.ahmadi <zabiullah.ahmadi@etu.hesge.ch>
2022-01-10 15:13:58 +0100

    comments added

--- lib.py
+++ lib.py
@@ -1,13 +1,13 @@
 def exp_rapide(x,pow,n): 
     result=1 
-    while pow!=0: 
-        if pow%2==1:
-            result = (result * x) % n
+    while pow!=0: # while pow is not zero 
+        if pow%2==1: # if the power is odd  like n^3 
+            result = (result * x) % n # 
         pow = pow // 2 # floor division 
         x = (x * x) % n 
     return result
 
-
+# find two primary number : p et q because p*q should equals n 
 def calc_p_q(n):
     nb_premier = n
     facteur  = 2
@@ -17,11 +17,11 @@ def calc_p_q(n):
         facteur += 1 
     return int(n/nb_premier), int(nb_premier)
 
-
+# calculate z = (p - 1) * (q - 1)
 def calc_z(n):
     p_q = []
     p_q = calc_p_q(n)
-    return (p_q[0] -1) *(p_q[1] - 1)
+    return (p_q[0] - 1) *(p_q[1] - 1)
 
 
 def euclide_etendu(dividende, diviseur):
@@ -55,8 +55,10 @@ def euclide_etendu(dividende, diviseur):
 
 
 def calc_private_key(public_key, n):
-
+    # z = (p - 1) * (q - 1) : p and q are two primary numbers 
     z = calc_z(n)
+
+    # return private key 
     return euclide_etendu(z, public_key)[1]
 
 
@@ -68,6 +70,7 @@ def decode_by_private_key(key, n, message_list):
     
     return decoded_message_by_private_key
 
+# decode raw message (65 => 'A', 66 => 'B')
 def decode_raw_data(raw_message_list):
     decoded_message = ""
     for msg in raw_message_list:
